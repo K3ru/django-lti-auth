9983cbd3170aa5d3a88157851364870276b8b338
Raquel Escalante <escalante.raquelj@gmail.com>
2019-11-08 20:41:53 -0400

    Changing valid_lti_request hooks redirect method

--- django_lti_auth/views.py
+++ django_lti_auth/views.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 from django.conf import settings
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.module_loading import import_string
 from django.views.decorators.csrf import csrf_exempt
@@ -20,7 +20,7 @@ def denied(r):
     return render(r, 'denied.html')
 
 @csrf_exempt
-def auth(request):   
+def auth(request):
     '''POST handler for the LTI login POST back call'''
     if request.method == 'POST':
         # Extracts the LTI payload information
@@ -32,7 +32,7 @@ def auth(request):
         # Extracts the request headers from the request
         headers = request.META
         # Get the default next URL from the LTI config
-        next_url = settings.PYLTI_CONFIG.get('next_url', get_reverse('admin:index'))
+        next_template = settings.PYLTI_CONFIG.get('next_url')
         try:
             # Validate the incoming LTI
             verify_request_common(consumers, url,\
@@ -44,10 +44,14 @@ def auth(request):
                 # If there is a return URL from the configured call the redirect URL
                 # is updated with the one that is returned. This is to enable redirecting to 
                 # constructed URLs
-                update_url = import_string(login_method_hook)(params, request)
-                if update_url:
-                    next_url = update_url
-            return HttpResponseRedirect(next_url)
+                ############ K3ru: updated lines ################################
+                update_template = import_string(login_method_hook)(params, request)
+                print("Update URL: ", update_template)
+                if update_template:
+                    next_template = update_template
+                    print("USER post_update: ", request.user.username)
+            return render(request, next_template)
+            ################### End updated lines ###############################
         except LTIException:
             # Map and call the invalid login method hook if defined in the settings
             invalid_login_method_hook = settings.PYLTI_CONFIG.get('method_hooks',{}).get('invalid_lti_request',None)
